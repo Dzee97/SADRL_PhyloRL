f935d591c98a543a1425639ce7b2750b246a5cc7
dzee <df.zee@hotmail.com>
2025-10-28 15:28:01 +0100

    log scale

--- evaluation.py
+++ evaluation.py
@@ -91,7 +91,8 @@ def plot_over_checkpoints(evaluate_dir: Path, dataset_name: str, algorithm_name:
 
         # Parsimony baseline
         ax1.axhline(y=pars_lls[sample_idx], color='gray', linestyle="--", linewidth=1.5, label="Parsimony LL")
-        ax1.set_ylim((pars_lls[sample_idx] - 5, pars_lls[sample_idx] + 2))
+        # ax1.set_ylim((pars_lls[sample_idx] - 5, pars_lls[sample_idx] + 2))
+        ax1.set_yscale('log')
 
         ax1.tick_params(axis='y', labelcolor=color)
         ax1.legend(loc='lower left', fontsize=9)
