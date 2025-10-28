4283110db4103259e3d6fb32d3bf1f040db05dd3
dzee <df.zee@hotmail.com>
2025-10-28 15:22:41 +0100

    new y lim

--- evaluation.py
+++ evaluation.py
@@ -91,6 +91,7 @@ def plot_over_checkpoints(evaluate_dir: Path, dataset_name: str, algorithm_name:
 
         # Parsimony baseline
         ax1.axhline(y=pars_lls[sample_idx], color='gray', linestyle="--", linewidth=1.5, label="Parsimony LL")
+        ax1.set_ylim((pars_lls[sample_idx] - 10, pars_lls[sample_idx] + 5))
 
         ax1.tick_params(axis='y', labelcolor=color)
         ax1.legend(loc='lower left', fontsize=9)
