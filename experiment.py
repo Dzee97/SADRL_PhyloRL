4034b4901e973a8287a52d917fc37087214fa7dc
dzee <df.zee@hotmail.com>
2025-11-27 12:46:57 +0100

    with 50 now

--- experiment.py
+++ experiment.py
@@ -32,10 +32,10 @@ sampling_cfg = dict(
 # Experiment sets
 EXPERIMENTS = {
     # Sample size 9
-    "Size9Samples1Train100Test20": dict(sample_size=9, num_samples=1,
-                                        num_rand_train_trees=100, num_rand_test_trees=20),
-    # "Size9Samples50Train100Test20": dict(sample_size=9, num_samples=50,
-    #                                     num_rand_train_trees=100, num_rand_test_trees=20),
+    # "Size9Samples1Train100Test20": dict(sample_size=9, num_samples=1,
+    #                                    num_rand_train_trees=100, num_rand_test_trees=20),
+    "Size9Samples50Train100Test20": dict(sample_size=9, num_samples=50,
+                                         num_rand_train_trees=100, num_rand_test_trees=20),
     # "Size9Samples100Train100Test20": dict(sample_size=9, num_samples=100,
     #                                      num_rand_train_trees=100, num_rand_test_trees=20),
     # "Size9Samples500Train100Test20": dict(sample_size=9, num_samples=500,
